ef2a07440476cf3c25cf83f7808b59abcc67ec25
Nguyen Nga <nguyennga200x@gmail.com>
2024-11-10 21:45:16 +0700

    Update folder test unit

---
+++ tests/test_unit/test_delete.py
@@ -0,0 +1,34 @@
+import unittest
+import csv
+from app import app
+
+class FlaskTestCase(unittest.TestCase):
+
+  def setUp(self):
+    self.app = app.test_client()
+    self.app.testing = True
+    self.test_data = self.load_test_data(r'D:\Nga\Nam4\Ki1_Nam4\dam-bao-chat-luong-va-kiem-thu-phan-mem\TEAMWORK\management-components-of-software-quality\todo-app\tests\test_unit\data\test_unit.csv')
+
+  def tearDown(self):
+    pass
+
+  def load_test_data(self, file_name):
+    data = []
+    with open(file_name, newline='') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+        data.append(row)
+    return data
+
+  def test_delete_task(self):
+      for task in self.test_data:
+          result = self.app.post(f"/deleteTask/{task['task_id']}")
+          self.assertEqual(result.status_code, 302)
+
+  def test_delete_completed_task(self):
+      for task in self.test_data:
+          result = self.app.post(f"/delete-completed/{task['task_id']}")
+          self.assertEqual(result.status_code, 302)
+
+if __name__ == '__main__':
+  unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(FlaskTestCase))
